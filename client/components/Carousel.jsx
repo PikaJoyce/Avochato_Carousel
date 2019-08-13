6c38cb3f6a912202635019aaddb67ffb6970e85c
Joyce Ma <pikajoyce@gmail.com>
2019-08-12 20:54:14 -0700

    Began working on jQuery carousel

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -2,30 +2,26 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 
 
-class App extends React.Component {
-  render() {
-    return (
-      <div style={{ padding: '10px', margin: 'auto-margin', fontSize: '1.5em' }}>
-        <Carousel infiniteLoop autoPlay swipeable={false} thumbWidth={1}>
-          <div>
-            <img src="http://lorempixel.com/output/food-q-c-400-200-1.jpg" />
-          </div>
-          <div>
-            <img src="http://lorempixel.com/output/food-q-c-400-200-2.jpg" />
-          </div>
-          <div>
-            <img src="http://lorempixel.com/output/food-q-c-400-200-3.jpg" />
-          </div>
-          <div>
-            <img src="http://lorempixel.com/output/food-q-c-400-200-4.jpg" />
-          </div>
-          <div>
-            <img src="http://lorempixel.com/output/food-q-c-400-200-5.jpg" />
-          </div>
-        </Carousel>
-      </div >
-    )
-  }
-}
+const App = () => (
+  <div style={{ padding: '10px', margin: 'auto-margin', fontSize: '1.5em' }}>
+    <Carousel infiniteLoop autoPlay swipeable={false} thumbWidth={1}>
+      <div>
+        <img src="http://lorempixel.com/output/food-q-c-400-200-1.jpg" />
+      </div>
+      <div>
+        <img src="http://lorempixel.com/output/food-q-c-400-200-2.jpg" />
+      </div>
+      <div>
+        <img src="http://lorempixel.com/output/food-q-c-400-200-3.jpg" />
+      </div>
+      <div>
+        <img src="http://lorempixel.com/output/food-q-c-400-200-4.jpg" />
+      </div>
+      <div>
+        <img src="http://lorempixel.com/output/food-q-c-400-200-5.jpg" />
+      </div>
+    </Carousel>
+  </div >
+);
 
 export default App;
