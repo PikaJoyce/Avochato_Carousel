e120f667e7cf7c1e68b0db90dde5ae59a5331b70
Joyce Ma <pikajoyce@gmail.com>
2019-08-12 17:38:01 -0700

    Part 1 Done

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import "../../node_modules/react-responsive-carousel/lib/styles/carousel.min.css";
 
 
 class App extends React.Component {
   render() {
     return (
-      <Carousel infiniteLoop autoPlay swipeable={false} thumbWidth={1}>
-        <div>
-          <img src="https://i.kym-cdn.com/entries/icons/original/000/009/803/spongebob-squarepants-patrick-spongebob-patrick-star-background-225039.jpg?crop=0.513xw:1.00xh;0.258xw,0&resize=480:*" />
-        </div>
-        <div>
-          <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mocking-spongebob-1556133078.jpg?crop=0.513xw:1.00xh;0.258xw,0&resize=480:*" />
-        </div>
-        <div>
-          <img src="https://i.kym-cdn.com/entries/icons/facebook/000/012/589/patrickstar.jpg?crop=0.513xw:1.00xh;0.258xw,0&resize=480:*" />
-        </div>
-        <div>
-          <img src="https://i.ytimg.com/vi/AeldQIpk2Ow/hqdefault.jpg?crop=0.513xw:1.00xh;0.258xw,0&resize=480:*" />
-        </div>
-        <div>
-          <img src="" />
-        </div>
-      </Carousel>
+      <div style={{ padding: '10px', margin: 'auto-margin', fontSize: '1.5em' }}>
+        <Carousel infiniteLoop autoPlay swipeable={false} thumbWidth={1}>
+          <div>
+            <img src="http://lorempixel.com/output/food-q-c-400-200-1.jpg" />
+          </div>
+          <div>
+            <img src="http://lorempixel.com/output/food-q-c-400-200-2.jpg" />
+          </div>
+          <div>
+            <img src="http://lorempixel.com/output/food-q-c-400-200-3.jpg" />
+          </div>
+          <div>
+            <img src="http://lorempixel.com/output/food-q-c-400-200-4.jpg" />
+          </div>
+          <div>
+            <img src="http://lorempixel.com/output/food-q-c-400-200-5.jpg" />
+          </div>
+        </Carousel>
+      </div >
     )
   }
 }
--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react'
 import App from './components/Carousel.jsx'
+import "../node_modules/react-responsive-carousel/lib/styles/carousel.min.css";
 
 ReactDOM.render(<App />, document.getElementById('app'));
