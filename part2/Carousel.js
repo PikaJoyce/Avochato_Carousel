aaf16bbc338b840f53efd809ad48fe702ef18894
Joyce Ma <pikajoyce@gmail.com>
2019-08-14 19:53:38 -0700

    Finished up vanilla carousel

--- part2/Carousel.js
+++ part2/Carousel.js
@@ -16,8 +16,12 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -28,3 +32,7 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+setInterval(() => {
+  plusSlides(1)
+}, 2000);
